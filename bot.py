4ba1b0a26bd26bb664f814ad2413355e7476101a
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2025-05-30 01:43:40 +0600

    test

--- bot.py
+++ bot.py
@@ -303,11 +303,11 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     try:
-        asyncio.run(main())
+        asyncio.get_event_loop().run_until_complete(main())
     except RuntimeError:
         import nest_asyncio
         nest_asyncio.apply()
-        asyncio.run(main())
+        asyncio.get_event_loop().run_until_complete(main())
 
 # if __name__ == "__main__":
 #     import asyncio
